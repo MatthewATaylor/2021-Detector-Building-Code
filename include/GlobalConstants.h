9dcbc08ea7b7eb2d1c5dafef490e06110134d4e0
Matthew Taylor <MatthewAlanTaylor@outlook.com>
2021-01-22 11:51:58 -0500

    Update fixed resistance

--- include/GlobalConstants.h
+++ include/GlobalConstants.h
@@ -6,10 +6,7 @@
 struct GlobalConstants {
 	static constexpr double KELVIN_CONVERSION = 273.15;
 	static constexpr double V_OUT = 3.3;
-
-	// Theoretical fixed resistance: 10000 ohms
-	// Resistance determined with multimeter (9940 ohms) is used
-	static constexpr double FIXED_RESISTANCE = 9940;
+	static constexpr double FIXED_RESISTANCE = 220000;
 };
 
 #endif
